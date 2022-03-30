f80ec4f318f473af2f6a0a0fd8b89f5be8eb18cb
Jack Humphries <jackphumphries02@gmail.com>
2022-03-30 22:43:23 +0100

    Coded the cli

---
+++ README.md
@@ -0,0 +1,33 @@
+<!-- PROJECT LOGO -->
+<br />
+<p align="center">
+
+  <h3 align="center">Create JH Article CLI</h3>
+
+</p>
+
+<!-- TABLE OF CONTENTS -->
+
+## Table of Contents
+
+-   [About the Project](#about-the-project)
+-   [Contributing](#contributing)
+-   [Contact](#contact)
+
+<!-- ABOUT THE PROJECT -->
+
+## About The Project
+
+This project is a small little project which I created that allows me to create the markdown template for my blog articles. Because I'm the only one using it, there is absolutely no error handling and it is written in vanilla Javascript.
+
+## Contributing
+
+I'm not really open to contributions for this project as it is a personal project. However, if you notice a mistake, an error or something you think that isn't right, consider opening an issue. Thank you!
+
+<!-- CONTACT -->
+
+## Contact
+
+Your Name - [@JackPHumphries](https://twitter.com/JackPHumphries) - me@jackhumphries.io
+
+Website Link: [https://jackhumphries.io](https://jackhumphries.io)
--- src/cli.js
+++ src/cli.js
@@ -1,1 +1,51 @@
-export async function cli(args) {}
+import readline from "readline";
+import fs from "fs";
+
+export function cli(args) {
+	console.log("Jack Humphries Blog Article Generator");
+
+	const rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout,
+	});
+
+	const curDate = new Date();
+
+	let answers = {
+		title: "",
+		slug: "",
+		excerpt: "[REPLACE ME]",
+		coverImage: "/images/default.jpg",
+		date: curDate.toISOString(),
+	};
+
+	rl.question("Title: ", (answer) => {
+		answers.title = answer;
+
+		rl.question("URL Slug: ", (answer) => {
+			answers.slug = answer;
+
+			rl.question("Cover Image URL: ", (answer) => {
+				if (answer.length >= 1) {
+					answers.coverImage = answer;
+				}
+
+				const fContents = `---
+title: "${answers.title}"
+excerpt: "${answers.excerpt}"
+coverImage: "${answers.coverImage}"
+date: "${answers.date}"
+ogImage:
+    url: "${answers.coverImage}"
+---
+
+Write your article here
+                `;
+
+				fs.writeFileSync(`./${answers.slug}.md`, fContents);
+
+				process.exit();
+			});
+		});
+	});
+}
